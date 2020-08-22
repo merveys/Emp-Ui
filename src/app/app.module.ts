56b11a5723c6db4ff7341d2cc6f05948c48faed5
Merve Ayaz <ayaz.merve1998@gmail.com>
2020-08-22 14:46:44 +0300

    Ui starting

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee/create-employee.component';
+import { RouterModule,Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'list', component: ListEmployeesComponent },
+  { path: 'create', component: CreateEmployeeComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListEmployeesComponent,
+    CreateEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
